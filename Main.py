bc78486e360d9e113533b2cfc1bea28e22c1d29d
Nnko1 <91994223+nanakoTakashi@users.noreply.github.com>
2022-06-02 10:54:41 +0300

    Update Main.py

--- Main.py
+++ Main.py
@@ -2,12 +2,17 @@
 import telebot
 from datatime import datatime
 
-#TOKEN
-TOKEN =""
-app = telebot.TeleBot('')
-
-#puplic var's
 now = datetime.now()
 
+#TOKEN
+TOKEN = '5353696413:AAG5PDs7YKW0FhW8HP6c0pUU6MJf4meyyoo'
+app = telebot.TeleBot(TOKEN)
+print("{now}: TOKEN is running")
 
 
+#puplic var's
+
+
+#polling
+
+app.polling()
