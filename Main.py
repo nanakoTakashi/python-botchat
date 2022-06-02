35ef175aea346b26b8e25a1bdb0ce75a4c03405a
Nnko1 <91994223+nanakoTakashi@users.noreply.github.com>
2022-06-02 12:17:30 +0300

    update Main.py

--- Main.py
+++ Main.py
@@ -1,8 +1,9 @@
 #imports
 import telebot
 from datatime import datatime
-
+#main puplic var's
 now = datetime.now()
+
 
 #TOKEN
 TOKEN = '5353696413:AAG5PDs7YKW0FhW8HP6c0pUU6MJf4meyyoo'
@@ -11,6 +12,13 @@ print("{now}: TOKEN is running")
 
 
 #puplic var's
+uid = message.from_user.id
+
+#mithods
+@app.message_handler(commands=["start"])
+def starting(message):
+  mci = message.chat.id
+  
 
 
 #polling
